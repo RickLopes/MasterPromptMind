15e1cf2fbe0b55cc856124ba84ff95d9b0b3e809
Hugo Baganha <hugobaganha@gmail.com>
2019-03-15 21:04:16 +0000

    Modified Server Branch

--- src/org/academiadecodigo/masterPromptMind/Server.java
+++ src/org/academiadecodigo/masterPromptMind/Server.java
@@ -1,10 +1,31 @@
 package org.academiadecodigo.masterPromptMind;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Server {
 
-    public static void main(String[] args) {
+    private ServerSocket serverSocket;
+    Client client;
+
+    private
+
+    public void Server() throws IOException {
+        serverSocket = new ServerSocket(8080);
+        client = new ConcurrentHashMap<>();
 
 
     }
 
+    public static void main(String[] args) {
+
+        Server server = new Server();
+        server.start();
+    }
+
+    public void start() {
+
+    }
 }
